524f3808aa71e35667e56cb4902673ef4b5606af
singularitti <singularitti@outlook.com>
2023-02-23 16:20:39 -0500

    Add `ElectronsEnergyPlot`

--- src/QuantumESPRESSORecipes.jl
+++ src/QuantumESPRESSORecipes.jl
@@ -1,5 +1,6 @@
 module QuantumESPRESSORecipes
 
+using DataFrames: groupby
 using QuantumESPRESSO.Outputs.PWscf
 using RecipesBase: @userplot, @recipe, @series
 
@@ -22,6 +23,29 @@ using RecipesBase: @userplot, @recipe, @series
         @series begin
             label --> string(columnnames[j])
             column
+        end
+    end
+end
+
+@userplot ElectronsEnergyPlot
+@recipe function f(plot::ElectronsEnergyPlot)
+    framestyle --> :box
+    legend_foreground_color --> nothing
+    grid --> nothing
+    seriestype --> :scatter
+    xguide --> "iterations"
+    yguide --> "energy"
+    df = first(plot.args)
+    columnnames = names(df)
+    for (i, subdf) in enumerate(groupby(df, :step))
+        for (j, column) in enumerate(eachcol(subdf))
+            if j in (1, 2)
+                continue
+            end
+            @series begin
+                label --> string(columnnames[j], " (iteration ", i, ')')
+                filter(!isnothing, column)
+            end
         end
     end
 end
