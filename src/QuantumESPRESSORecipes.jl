f61628175c5adc4a918e21f8ad7f06a8285c8016
singularitti <singularitti@outlook.com>
2023-02-23 15:55:50 -0500

    Add `DecomposedEnergyPlot`

--- src/QuantumESPRESSORecipes.jl
+++ src/QuantumESPRESSORecipes.jl
@@ -1,5 +1,29 @@
 module QuantumESPRESSORecipes
 
-# Write your package code here.
+using QuantumESPRESSO.Outputs.PWscf
+using RecipesBase: @userplot, @recipe, @series
+
+@userplot DecomposedEnergyPlot
+@recipe function f(plot::DecomposedEnergyPlot)
+    framestyle --> :box
+    legend_foreground_color --> nothing
+    grid --> nothing
+    seriestype --> :scatter
+    xguide --> "steps"
+    yguide --> "energy"
+    df = first(plot.args)
+    steps = df[:, 1]
+    xticks --> steps
+    columnnames = names(df)
+    for (j, column) in enumerate(eachcol(df))
+        if j == 1
+            continue
+        end
+        @series begin
+            label --> string(columnnames[j])
+            column
+        end
+    end
+end
 
 end
